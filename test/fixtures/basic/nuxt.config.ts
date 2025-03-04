0ea6c0f80d8bf648bf9b1f20a551360f1613f4ca
Thomerson Roncally Araújo Teixeira <thomersonroncally@outlook.com>
2025-03-04 17:02:28 -0400

    test: Mock important variables to run tests correctly

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -1,7 +1,16 @@
-import MyModule from '../../../src/module'
+import { resolve } from 'node:path'
 
 export default defineNuxtConfig({
   modules: [
-    MyModule,
+    'nuxt-growthbook',
   ],
+  alias: {
+    'nuxt-growthbook': resolve(__dirname, '../../../src/module.ts'),
+  },
+  growthbook: {
+    enabled: true,
+    apiHost: 'http://localhost:4100',
+    clientKey: 'sdk-XXXXXXXXXXXXX',
+    enableDevMode: true,
+  },
 })
